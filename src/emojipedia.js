313b1e65d7ac93de948c57bfecc1f3ca199a83fb
chiranjeev13 <44394108+chiranjeev13@users.noreply.github.com>
2022-08-15 12:03:12 +0530

    Added an emoji

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -26,7 +26,14 @@ const emojipedia = [
       name: "Naughty Mind",
       meaning:
         "Funny person who takes life as chill and never has any tension , funky mind "
-    }
+    },
+     {
+      id: 5,
+      emoji: "🥹",
+      name: "smiling face holding back tears",
+      meaning:
+        "A person who is so happy that his eyes are filled with tears "
+    
   ];
   export default emojipedia;
   
